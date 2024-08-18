8d6edf44c4f7defd3b42ca0f1fb89843f3645782
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-08-18 22:39:44 +0100

    chore: Comment out unused code in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/Header'
-import { Toaster } from '@/components/ui/sonner'
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+// import './globals.css'
+// import Header from '@/components/Header'
+// import { Toaster } from '@/components/ui/sonner'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Blogger App',
-  description:
-    'A blog app that uses AI to create the blog content based on prompts'
-}
+// export const metadata: Metadata = {
+//   title: 'Blogger App',
+//   description:
+//     'A blog app that uses AI to create the blog content based on prompts'
+// }
 
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-        <Toaster position='top-right' theme='light' richColors />
-      </body>
-    </html>
-  )
-}
+// export default function RootLayout({
+//   children
+// }: Readonly<{
+//   children: React.ReactNode
+// }>) {
+//   return (
+//     <html lang='en'>
+//       <body className={inter.className}>
+//         <Header />
+//         <main>{children}</main>
+//         <Toaster position='top-right' theme='light' richColors />
+//       </body>
+//     </html>
+//   )
+// }
