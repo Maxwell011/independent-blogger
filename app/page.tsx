ebf42243fec1ffd4ce1214dbee468248c5abfa9e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-08-18 22:38:41 +0100

    Comment out unused code in action.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Form from '@/components/Form'
+// import Form from '@/components/Form'
 
-export default function Home() {
-  return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>AI Blogger Site</h1>
-        <Form />
-      </div>
-    </section>
-  )
-}
+// export default function Home() {
+//   return (
+//     <section className='py-24'>
+//       <div className='container'>
+//         <h1 className='text-3xl font-bold'>AI Blogger Site</h1>
+//         <Form />
+//       </div>
+//     </section>
+//   )
+// }
